e380211b6789eab7ceca7f1d1e5c53a8b53a3378
Francis Chang <francis.hongkong@gmail.com>
2025-05-30 22:56:45 +0800

    update

---
+++ demo-sb-helloworld/src/main/java/com/bootcamp/demo/demo_sb_helloworld/HelloworldController.java
@@ -0,0 +1,29 @@
+package com.bootcamp.demo.demo_sb_helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@ResponseBody
+public class HelloworldController {
+
+  // ! Spring Boot default is 8080
+  // http://127.0.0.1:8080/helloworld
+  // Broswer (Client) -> Spring boot app (Server)
+  // Step 1: Compile Java Project (Spring Boot)
+  // Step 2: Jar
+  // Step 3: Start Server
+
+  // API
+  @GetMapping(value = "/helloworld")
+  public String helloworld() {
+    String s = "Hello World, welcome to Spring Boot.";
+
+    return s;
+  }
+
+  // create a new spring boot project
+  // "http://localhost:8080/person"
+  // creat a API to return a person (Person.class)-> John, 18
+}
